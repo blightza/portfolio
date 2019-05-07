799b8beaeb4ff8b575584a7d6012e6c1c2b7cc3e
blightza <bradhurflix@gmail.com>
2019-05-07 09:59:08 +0000

    fixed some grammar

--- middleware/index.js
+++ middleware/index.js
@@ -45,7 +45,7 @@ middlewareObj.isLoggedIn = function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
         return next();
     }
-    req.flash("error", "You neen to be signed in to do that")
+    req.flash("error", "You need to be signed in to do that")
     res.redirect("/login")
 };
   
